8f06868d765f04072d9bec2fe396105c2b69f969
Quanquan1906 <doquan19062004@gmail,com>
2025-06-14 15:26:23 +0700

    setup project react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+function App() {
+ 
+
+  return (
+    <div className={styles.App}>
+      Hello, this is my portfolio website built with React.
+    </div>
+    
+  )
+}
+
+export default App
