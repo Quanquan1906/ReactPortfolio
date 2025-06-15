7303df4d53a6db6f5c84c7734fced721e83927ea
Quanquan1906 <doquan19062004@gmail,com>
2025-06-16 01:46:59 +0700

    fix utils.js

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import styles from './Navbar.module.css';
+import { getImageUrl } from '../../utils';
 
 export const Navbar = () => { 
+    console.log(getImageUrl("nav/menuIcon.png")); 
     return (
         <nav className = {styles.navbar}>
             <a className = {styles.title} href ="/">Portfolio</a>
             <div className = {styles.menu}>
+                <img className = {styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="Menu Icon" />
                 <ul className = {styles.menuitems}>
                     <li>
                         <a href="#about">About</a>
